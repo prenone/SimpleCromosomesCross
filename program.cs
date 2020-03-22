e3f6f98d519eb609ea2c59394980038772ea744d
prenone <achillemerendino@outlook.com>
2020-03-22 21:41:31 +0100

    Added program.cs

---
+++ program.cs
@@ -0,0 +1,17 @@
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string g1 = "XY";
+        string g2 = "XX";
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                Console.Write(g1[i]);
+                Console.Write(g2[j]);
+            }
+        }
+    }
+}
